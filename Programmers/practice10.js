a5128508644d2dcd59197b3b56f56d579f29bb5c
jwhan <gksjeongw96@gmail.com>
2020-06-15 20:48:57 +0900

    Fix typo

--- Programmers/practice10.js
+++ Programmers/practice10.js
@@ -2,7 +2,7 @@
 function solution(board) {
     let max = 0;
 
-    if (board.length < 2 || board[0].length < 1) {
+    if (board.length < 2 || board[0].length < 2) {
         for (let i=0; i<board.length; i++) {
             for (let j=0; j<board[0].length; j++) {
                 if (board[i][j] === 1) max = 1;
